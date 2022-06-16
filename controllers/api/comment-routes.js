59efa927670dfcd46a56a482cce247463735a9e8
Jay Ford <jay.ford45@gmail.com>
2022-06-16 16:42:00 -0400

    added user and comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const { Comment } = require("../../models/");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, (req, res) => {
+    const body = req.body;
+    Comment.create({ ...body, userId: req.session.userId })
+    .then(newComment => {
+      res.json(newComment);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbCommentData => {
+        if (!dbCommentData) {
+          res.status(404).json({ message: 'No comment found with this id!' });
+          return;
+        }
+        res.json(dbCommentData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
